4fe4bc51d2734b406af607bcb272ed743ba1c369
rahul0679 <rahulcri98@gmail.com>
2025-07-13 15:07:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
